2d916dd04594fb7bee2e49bfa2624aee9f6d86c0
njange <jamesnjange80@gmail.com>
2024-06-12 18:12:49 +0300

    coding the server

---
+++ livestreamapp.java
@@ -0,0 +1,33 @@
+public class livestreamapp {
+    
+}
+
+    class Video {
+
+        public static int frameTime = 10;
+
+        String id;
+        Frame[] frames;
+        String jsonMetaData;
+
+        public Frame getFrame(int timestamp) {
+            for (int i=0; i<frames.length; i++) {
+                if (frames[i].timestamp <= timestamp &&
+                frames[i].timestamp+10 > timestamp) {
+                    return frames[i];
+                }
+        }
+
+        throw new IndexOutOfBoundsException();
+    }
+}
+
+
+    class Frame {
+        byte[] bytes;
+        int timestamp;
+    }
+
+    class User {
+        String id;
+    }
